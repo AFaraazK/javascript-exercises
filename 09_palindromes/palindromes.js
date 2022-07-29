c4557627c3641fa0e194c8120176bf6bad7e34e0
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2022-07-29 09:42:20 -0400

    Complete Exercise 09: Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    // remove upper cases
+    let newString = string.toLowerCase();
+    // remove punctuation
+    newString = newString.replace(/[^\w\s\']|_/g, "")
+           .replace(/\s+/g, "");
+    
+    // reverse the string
+    let reverseString = '';
+    for (let i = 0; i < newString.length; i++) {
+      reverseString = newString[i] + reverseString;
+    }
+    return reverseString == newString;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
