133229d17c0c454b391e6d634b5bbbbf4ed10dcf
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2022-08-07 15:23:23 -0400

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    let newArray = array.sort((a,b) => {
+      if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+    return newArray[0].name;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
